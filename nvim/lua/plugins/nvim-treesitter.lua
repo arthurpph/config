c03448857cfa97a58cdb57246d0832325b840853
arthurpph <arthurpiana13@gmail.com>
2026-02-26 00:43:11 -0300

    added nvim-treesitter experimental indentation

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -15,7 +15,10 @@ return {
         })
         vim.api.nvim_create_autocmd('FileType', {
             pattern = { '<filetype>' },
-            callback = function() vim.treesitter.start() end,
+            callback = function()
+                vim.treesitter.start()
+                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            end,
         })
     end
 }
